87237dd70a0fcf8da8ad5acd027cb00534d281fb
2mik <m_shir@mail.ru>
2015-07-21 15:36:38 +0300

    Add modern about form to Scheme Editor

--- ScadaScheme/ScadaSchemeEditor/FrmAbout.cs
+++ ScadaScheme/ScadaSchemeEditor/FrmAbout.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2014 Mikhail Shiryaev
+ * Copyright 2015 Mikhail Shiryaev
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,11 +20,13 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2013
- * Modified : 2014_
+ * Modified : 2015
  */
 
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
+using Utils;
 
 namespace Scada.Scheme.Editor
 {
@@ -34,15 +36,90 @@ namespace Scada.Scheme.Editor
     /// </summary>
     public partial class FrmAbout : Form
     {
-        public FrmAbout()
+        private const string Version = "4.5.0.0"; // версия приложения
+        private static FrmAbout frmAbout = null;  // форма о программе
+
+        private string exeDir;  // директория исполняемого файла приложения
+        private Log errLog;     // журнал ошибок приложения
+        private bool inited;    // форма инициализирована
+        private string linkUrl; // гиперссылка
+
+
+        /// <summary>
+        /// Конструктор, ограничивающий создание объекта без параметров
+        /// </summary>
+        private FrmAbout()
         {
             InitializeComponent();
+
+            inited = false;
+            linkUrl = "";
         }
+
+
+        /// <summary>
+        /// Отобразить форму о программе
+        /// </summary>
+        public static void ShowAbout(string exeDir, Log errLog)
+        {
+            if (exeDir == null)
+                throw new ArgumentNullException("exeDir");
+            if (errLog == null)
+                throw new ArgumentNullException("errLog");
+
+            if (frmAbout == null)
+            {
+                frmAbout = new FrmAbout();
+                frmAbout.exeDir = exeDir;
+                frmAbout.errLog = errLog;
+            }
+            frmAbout.ShowDialog();
+        }
+
 
         private void FrmAbout_Load(object sender, EventArgs e)
         {
-            pictureBox.Visible = Localization.UseRussian;
-            pictureBoxEn.Visible = !Localization.UseRussian;
+            // инициализация формы
+            if (!inited)
+            {
+                inited = true;
+
+                // настройка элементов управления в зависимости от локализации
+                PictureBox activePictureBox;
+
+                if (Localization.UseRussian)
+                {
+                    activePictureBox = pbAboutRu;
+                    pbAboutEn.Visible = false;
+                    lblVersionEn.Visible = false;
+                    lblVersionRu.Text = "Версия " + Version;
+                }
+                else
+                {
+                    activePictureBox = pbAboutEn;
+                    pbAboutRu.Visible = false;
+                    lblVersionRu.Visible = false;
+                    lblVersionEn.Text = "Version " + Version;
+                }
+
+                // загрузка изображения и гиперссылки из файлов, если они существуют
+                bool imgLoaded;
+                string errMsg;
+                if (ScadaUtils.LoadAboutForm(exeDir, this, activePictureBox, lblWebsite,
+                    out imgLoaded, out linkUrl, out errMsg))
+                {
+                    if (imgLoaded)
+                    {
+                        lblVersionRu.Visible = false;
+                        lblVersionEn.Visible = false;
+                    }
+                }
+                else
+                {
+                    errLog.WriteAction(errMsg);
+                    ScadaUtils.ShowError(errMsg);
+                }
+            }
         }
 
         private void FrmAbout_Click(object sender, EventArgs e)
@@ -53,6 +130,15 @@ namespace Scada.Scheme.Editor
         private void FrmAbout_KeyPress(object sender, KeyPressEventArgs e)
         {
             Close();
+        }
+
+        private void lblLink_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(linkUrl))
+            {
+                Process.Start(linkUrl);
+                Close();
+            }
         }
     }
 }
