da3be5f524d77b2dc9363c96ae15d7fc8e0e3507
2mik <m_shir@mail.ru>
2015-08-07 00:45:28 +0300

    Minor fixes 2

--- ScadaComm/ScadaCommSvc/Properties/AssemblyInfo.cs
+++ ScadaComm/ScadaCommSvc/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Rapid SCADA")]
-[assembly: AssemblyCopyright("Copyright © 2006-2014")]
+[assembly: AssemblyCopyright("Copyright © 2006-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
