93d5ebe82be73f68ea75f0f2c66ef5e968df172b
2mik <m_shir@mail.ru>
2015-08-21 01:07:13 +0300

    Fix master page title image

--- ScadaWeb/ScadaWeb/MasterLight.master.designer.cs
+++ ScadaWeb/ScadaWeb/MasterLight.master.designer.cs
@@ -31,6 +31,15 @@ namespace Scada.Web {
         protected global::System.Web.UI.HtmlControls.HtmlForm ScadaLightForm;
         
         /// <summary>
+        /// imgTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgTitle;
+        
+        /// <summary>
         /// contLight control.
         /// </summary>
         /// <remarks>
