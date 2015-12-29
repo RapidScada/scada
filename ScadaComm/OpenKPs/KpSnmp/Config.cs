4b3e7922578fe1c21c478ba976cd9197a81e51c5
2mik <m_shir@mail.ru>
2015-12-30 01:09:48 +0300

    Development of KpSnmp

---
+++ ScadaComm/OpenKPs/KpSnmp/Config.cs
@@ -0,0 +1,176 @@
+﻿/*
+ * Copyright 2014 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : KpSnmp
+ * Summary  : Device communication configuration
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2015
+ * Modified : 2015
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scada.Comm.Devices.KpSnmp
+{
+    /// <summary>
+    /// Device communication configuration
+    /// <para>Конфигурация связи с КП</para>
+    /// </summary>
+    internal class Config
+    {
+        /// <summary>
+        /// Группа переменных
+        /// </summary>
+        public class Group
+        {
+            /// <summary>
+            /// Конструктор
+            /// </summary>
+            public Group()
+            {
+                Name = "";
+                Variables = new List<Variable>();
+            }
+
+            /// <summary>
+            /// Получить или установить имя группы
+            /// </summary>
+            public string Name { get; set; }
+            /// <summary>
+            /// Получить переменные
+            /// </summary>
+            public List<Variable> Variables { get; private set; }
+        }
+
+        /// <summary>
+        /// Переменная
+        /// </summary>
+        public class Variable
+        {
+            /// <summary>
+            /// Конструктор
+            /// </summary>
+            public Variable()
+            {
+                Name = "";
+                OID = "";
+            }
+
+            /// <summary>
+            /// Получить или установить имя переменной
+            /// </summary>
+            public string Name { get; set; }
+            /// <summary>
+            /// Получить или установить идентификатор переменной
+            /// </summary>
+            public string OID { get; set; }
+        }
+
+        /// <summary>
+        /// Пароль на чтение данных по умоланию
+        /// </summary>
+        public const string DefReadCommunity = "public";
+        /// <summary>
+        /// Пароль на запись данных по умоланию
+        /// </summary>
+        public const string DefWriteCommunity = "private";
+
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public Config()
+        {
+            SetToDefault();
+        }
+
+
+        /// <summary>
+        /// Получить или установить пароль на чтение данных
+        /// </summary>
+        public string ReadCommunity { get; set; }
+
+        /// <summary>
+        /// Получить или установить пароль на запись данных
+        /// </summary>
+        public string WriteCommunity { get; set; }
+
+        /// <summary>
+        /// Получить граппы переменных
+        /// </summary>
+        public List<Group> Groups { get; private set; }
+
+
+        /// <summary>
+        /// Установить значения параметров конфигурации по умолчанию
+        /// </summary>
+        private void SetToDefault()
+        {
+            ReadCommunity = DefReadCommunity;
+            WriteCommunity = DefWriteCommunity;
+            Groups = new List<Group>();
+        }
+        
+        /// <summary>
+        /// Загрузить конфигурацию из файла
+        /// </summary>
+        public bool Load(string fileName, out string errMsg)
+        {
+            SetToDefault();
+
+            try
+            {
+                errMsg = "";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errMsg = CommPhrases.LoadKpSettingsError + ":\r\n" + ex.Message;
+                return false;
+            }
+        }
+
+        
+        /// <summary>
+        /// Сохранить конфигурацию в файле
+        /// </summary>
+        public bool Save(string fileName, out string errMsg)
+        {
+            try
+            {
+                errMsg = "";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errMsg = CommPhrases.SaveKpSettingsError + ":\r\n" + ex.Message;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Получить имя файла конфигурации
+        /// </summary>
+        public static string GetFileName(string configDir, int kpNum)
+        {
+            return configDir + "KpSnmp_" + CommUtils.AddZeros(kpNum, 3) + ".xml";
+        }
+    }
+}
--- ScadaComm/OpenKPs/KpSnmp/KpSnmpView.cs
+++ ScadaComm/OpenKPs/KpSnmp/KpSnmpView.cs
@@ -1,10 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*
+ * Copyright 2015 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : KpSnmp
+ * Summary  : Device library user interface
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2015
+ * Modified : 2015
+ */
 
 namespace Scada.Comm.Devices
 {
-    public class KpSnmpView
+    /// <summary>
+    /// Device library user interface
+    /// <para>Пользовательский интерфейс библиотеки КП</para>
+    /// </summary>
+    public class KpSnmpView : KPView
     {
+        /// <summary>
+        /// Конструктор для общей настройки библиотеки КП
+        /// </summary>
+        public KpSnmpView()
+            : this(0)
+        {
+        }
+
+        /// <summary>
+        /// Конструктор для настройки конкретного КП
+        /// </summary>
+        public KpSnmpView(int number)
+            : base(number)
+        {
+            CanShowProps = true;
+        }
+
+
+        /// <summary>
+        /// Описание библиотеки КП
+        /// </summary>
+        public override string KPDescr
+        {
+            get
+            {
+                return Localization.UseRussian ?
+                    "Взаимодействие с контроллерами по протоколу SNMP v2c.\n\n" +
+                    "Команды ТУ:\n" +
+                    "Отдельная команда для установки каждой переменной (стандартная или бинарная). " + 
+                    "Номер команды равен номеру сигнала КП." :
+
+                    "Interacting with controllers via SNMP v2c protocol.\n\n" +
+                    "Commands:\n" +
+                    "A separate command for setting each variable (standard or binary). " +
+                    "Command number is equal to a signal number of a device tag.";
+            }
+        }
     }
 }
