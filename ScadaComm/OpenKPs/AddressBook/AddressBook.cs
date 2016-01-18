f08afee69f7265338d1a6d056d47a1ca17b4a6d1
2mik <m_shir@mail.ru>
2016-01-19 00:57:14 +0300

    Initial commit of AddressBook and KpEmail

---
+++ ScadaComm/OpenKPs/AddressBook/AddressBook.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Copyright 2016 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : AddressBook
+ * Summary  : Address book
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2015
+ * Modified : 2015
+ */
+
+using System.Collections.Generic;
+
+namespace Scada.Comm.Devices.AddressBook
+{
+    /// <summary>
+    /// Address book
+    /// <para>Адресная книга</para>
+    /// </summary>
+    public class AddressBook
+    {
+        /// <summary>
+        /// Группа контактов
+        /// </summary>
+        public class ContactGroup
+        {
+
+        }
+
+        /// <summary>
+        /// Контакт
+        /// </summary>
+        public class Contact
+        {
+
+        }
+
+        /// <summary>
+        /// Телефонный номер
+        /// </summary>
+        public class PhoneNumber
+        {
+
+        }
+
+        /// <summary>
+        /// Адрес электронной почты
+        /// </summary>
+        public class Email
+        {
+
+        }
+
+
+        /// <summary>
+        /// Имя файла адресной книги по умолчанию
+        /// </summary>
+        public const string DefFileName = "AddressBook.xml";
+
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public AddressBook()
+        {
+            ContactGroups = new SortedList<string, ContactGroup>();
+        }
+
+
+        /// <summary>
+        /// Получить группы телефонных контактов, упорядоченные по наименованию
+        /// </summary>
+        public SortedList<string, ContactGroup> ContactGroups { get; private set; }
+    }
+}
---
+++ ScadaComm/OpenKPs/KpEmail/KpEmailView.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Copyright 2016 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : KpEmail
+ * Summary  : Device library user interface
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2016
+ * Modified : 2016
+ */
+
+namespace Scada.Comm.Devices
+{
+    /// <summary>
+    /// Device library user interface
+    /// <para>Пользовательский интерфейс библиотеки КП</para>
+    /// </summary>
+    public class KpEmailView : KPView
+    {
+        /// <summary>
+        /// Конструктор для общей настройки библиотеки КП
+        /// </summary>
+        public KpEmailView()
+            : this(0)
+        {
+        }
+
+        /// <summary>
+        /// Конструктор для настройки конкретного КП
+        /// </summary>
+        public KpEmailView(int number)
+            : base(number)
+        {
+            CanShowProps = true;
+        }
+
+        /// <summary>
+        /// Описание библиотеки КП
+        /// </summary>
+        public override string KPDescr
+        {
+            get
+            {
+                return Localization.UseRussian ?
+                    "Отправка уведомлений по электронной почте.\n\n" +
+                    "Команды ТУ:\n" +
+                    "1 (бинарная) - отправка уведомления.\n\n" +
+                    "Примеры текста команды:\n" +
+                    "group_name;subject;message\n" +
+                    "contact_name;subject;message\n" +
+                    "email;subject;message" :
+
+                    "Sending email notifications.\n\n" +
+                    "Commands:\n" +
+                    "1 (binary) - send the notification.\n\n" +
+                    "Command text examples:\n" +
+                    "group_name;subject;message\n" +
+                    "contact_name;subject;message\n" +
+                    "email;subject;message";
+            }
+        }
+
+        /// <summary>
+        /// Отобразить свойства КП
+        /// </summary>
+        public override void ShowProps()
+        {
+            // отображение адресной книги
+            //FrmPhonebook.ShowDialog(AppDirs);
+        }
+    }
+}
