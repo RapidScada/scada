435643e5e75489db939b2dd4d7f5b25dc40abc0a
2mik <m_shir@mail.ru>
2016-03-02 17:14:26 +0300

    ScadaWeb5Beta: start creating js api

---
+++ ScadaWeb/ScadaWebShell5Beta/js/api/scada-api.js
@@ -0,0 +1,38 @@
+﻿/*
+ * Rapid SCADA API for access data and send commands
+ * Rapid SCADA API для доступа к данным и отправки команд
+ *
+ * Author   : Mikhail Shiryaev
+ * Created  : 2016
+ * Modified : 2016
+ */
+
+// Namespace
+var scada = scada || {};
+
+// Объект API
+scada.api = {
+    // Получить текущее значение входного канала
+    getCnlVal: function (cnlNum, callback) {
+        callback(0.0);
+    },
+
+    // Получить текущий статус входного канала
+    getCnlStat: function (cnlNum, callback) {
+        callback(0);
+    },
+
+    // Получить текущие данные входного канала 
+    getCnlData: function (cnlNum, callback) {
+        callback(new scada.CnlDataView());
+    }
+};
+
+// Тип данных входного канала
+scada.CnlDataView = function () {
+    this.val = 0.0;
+    this.stat = 0;
+    this.text = "";
+    this.textWithUnit = "";
+    this.color = "";
+};
