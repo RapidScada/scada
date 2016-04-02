5093655f1560d94a809292535e01d3aa6e53098c
2mik <m_shir@mail.ru>
2016-04-02 17:36:18 +0300

    ScadaAdmin: save window state fix

--- ScadaAdmin/ScadaAdmin/AppCode/Settings.cs
+++ ScadaAdmin/ScadaAdmin/AppCode/Settings.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2015 Mikhail Shiryaev
+ * Copyright 2016 Mikhail Shiryaev
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2010
- * Modified : 2015
+ * Modified : 2016
  */
 
 using System;
@@ -351,7 +351,6 @@ namespace ScadaAdmin
                 rootElem.AppendParamElem("Height", FormSt.Height);
                 rootElem.AppendParamElem("Maximized", FormSt.Maximized);
                 rootElem.AppendParamElem("ExplorerWidth", FormSt.ExplorerWidth);
-                rootElem.AppendParamElem("Left", FormSt.Left);
 
                 // сохранение в файле
                 xmlDoc.Save(AppData.ExeDir + FormStateFileName);
