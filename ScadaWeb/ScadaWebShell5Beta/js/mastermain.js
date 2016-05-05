948f8e12e5b3f64697b212a1b24769f5e563b0a7
2mik <m_shir@mail.ru>
2016-05-05 19:18:21 +0300

    ScadaWeb5: full screen mode

--- ScadaWeb/ScadaWebShell5Beta/js/mastermain.js
+++ ScadaWeb/ScadaWebShell5Beta/js/mastermain.js
@@ -1,5 +1,12 @@
-﻿// Update layout of the master page
-function updateMainLayout() {
+﻿var scada = scada || {};
+scada.masterMain = scada.masterMain || {};
+scada.masterMain.params = scada.masterMain.params || {};
+
+// Left pane that displays main menu or views explorer is expanded
+scada.masterMain.leftPaneExpanded = true;
+
+// Update layout of the master page
+scada.masterMain.updateLayout = function () {
     var divMainHeader = $("#divMainHeader");
     var divMainLeftPane = $("#divMainLeftPane");
     var divMainTabs = $("#divMainTabs");
@@ -7,19 +14,19 @@ function updateMainLayout() {
     var paneH = $(window).height() - divMainHeader.outerHeight();
     divMainLeftPane.outerHeight(paneH);
     divMainTabs.outerWidth(paneH);
-}
+};
 
 // Choose a tool window and activate it
-function chooseToolWindow() {
-    if (mainMenuVisible) {
-        activateToolWindow($("#divMainMenuTab"));
+scada.masterMain.chooseToolWindow = function () {
+    if (this.params.mainMenuVisible) {
+        this.activateToolWindow($("#divMainMenuTab"));
     } else {
-        activateToolWindow($("#divMainExplorerTab"));
+        this.activateToolWindow($("#divMainExplorerTab"));
     }
-}
+};
 
 // Make visible main menu or views explorer
-function activateToolWindow(divClickedTab) {
+scada.masterMain.activateToolWindow = function (divClickedTab) {
     // highlight clicked tab
     var tabs = $("#divMainTabs .tab");
     tabs.removeClass("selected");
@@ -44,20 +51,105 @@ function activateToolWindow(divClickedTab) {
     if (toolWindow) {
         toolWindow.css("display", "block");
     }
-}
+};
+
+// Hide main menu and views explorer
+scada.masterMain.hideLeftPane = function () {
+    $("#divMainLeftPane").css("display", "none");
+    $("body").css("padding-left", "0");
+};
+
+// Show main menu and views explorer
+scada.masterMain.showLeftPane = function () {
+    $("body").css("padding-left", "");
+    $("#divMainLeftPane").css("display", "");
+};
+
+// Collapse main menu and views explorer
+scada.masterMain.collapseLeftPane = function () {
+    $("#spanMainShowMenu").css("display", "inline-block");
+    this.hideLeftPane();
+    leftPaneExpanded = false;
+};
+
+// Expand main menu and views explorer
+scada.masterMain.expandLeftPane = function () {
+    this.showLeftPane();
+    $("#spanMainShowMenu").css("display", "none");
+    leftPaneExpanded = true;
+};
+
+// Hide page header
+scada.masterMain.hideHeader = function () {
+    $("#divMainHeader").css("display", "none");
+    $("body").css("padding-top", "0");
+};
+
+// Show page header
+scada.masterMain.showHeader = function () {
+    $("#divMainHeader").css("display", "");
+    $("body").css("padding-top", "");
+};
+
+// Hide all the menus and switch browser to fullscreen mode
+scada.masterMain.switchToFullscreen = function () {
+    if (leftPaneExpanded) {
+        this.hideLeftPane();
+    }
+    this.hideHeader();
+    $("#divMainNormalView").css("display", "inline-block");
+    scada.utils.requestFullscreen();
+};
+
+// Show all the menus and exit browser fullscreen mode
+scada.masterMain.switchToNormalView = function () {
+    $("#divMainNormalView").css("display", "none");
+    if (leftPaneExpanded) {
+        this.showLeftPane();
+    }
+    this.showHeader();
+    scada.utils.exitFullscreen();
+};
 
 $(document).ready(function () {
-    updateMainLayout();
-    chooseToolWindow();
+    scada.masterMain.updateLayout();
+    scada.masterMain.chooseToolWindow();
     scada.treeView.prepare();
 
     // update layout on window resize
     $(window).resize(function () {
-        updateMainLayout();
+        scada.masterMain.updateLayout();
     });
 
-    // activate tool window on tab click
-    $("#divMainTabs .tab").click(function (event) {
-        activateToolWindow($(this));
+    // activate tool window if tab is clicked
+    $("#divMainTabs .tab").click(function () {
+        scada.masterMain.activateToolWindow($(this));
+    });
+
+    // collapse left pane on button click
+    $("#divMainCollapsePane").click(function () {
+        scada.masterMain.collapseLeftPane();
+    });
+
+    // expand left pane on button click
+    $("#spanMainShowMenu").click(function () {
+        scada.masterMain.expandLeftPane();
+    });
+
+    // switch to full screen mode on button click
+    $("#spanMainFullScreen").click(function () {
+        scada.masterMain.switchToFullscreen();
+    });
+
+    // switch to normal view mode on button click
+    $("#divMainNormalView").click(function () {
+        scada.masterMain.switchToNormalView();
+    });
+
+    // show menus if user exits fullscreen mode
+    $(document).on("webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange", function () {
+        if (!scada.utils.isFullScreen) {
+            scada.masterMain.switchToNormalView();
+        }
     });
 });
--- ScadaWeb/ScadaWebShell5Beta/js/scadautils.js
+++ ScadaWeb/ScadaWebShell5Beta/js/scadautils.js
@@ -32,5 +32,47 @@ scada.utils = {
     logFailedRequest: function (operation, jqXHR) {
         console.error(this.getCurTime() + " Request '" + operation + "' failed: " +
             jqXHR.status + " (" + jqXHR.statusText + ")");
+    },
+
+    // Check if browser is in fullscreen mode
+    // See https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
+    get isFullScreen() {
+        return document.fullscreenElement || document.mozFullScreenElement ||
+            document.webkitFullscreenElement || document.msFullscreenElement;
+    },
+
+    // Switch browser to fullscreen mode
+    requestFullscreen: function () {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    },
+
+    // Exit browser fullscreen mode
+    exitFullscreen: function () {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    },
+
+    // Switch browser to full screen mode and back to normal view
+    toggleFullscreen: function () {
+        if (this.isFullScreen) {
+            this.exitFullscreen();
+        } else {
+            this.requestFullscreen();
+        }
     }
 };
