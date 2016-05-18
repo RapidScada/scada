9d8247638a7be790768e90156e6794beb72fba87
2mik <m_shir@mail.ru>
2016-05-18 13:26:58 +0300

    ScadaWeb5: view hub or api

--- ScadaWeb/ScadaWebShell5Beta/View.aspx
+++ ScadaWeb/ScadaWebShell5Beta/View.aspx
@@ -1,7 +1,7 @@
 ﻿<%@ Page Title="Views - Rapid SCADA" Language="C#" MasterPageFile="~/MasterMain.Master" AutoEventWireup="true" CodeBehind="View.aspx.cs" Inherits="Scada.Web.WFrmView" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="cphMainHead" runat="server">
     <link href="css/view.min.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="js/splitter.js"></script>
+    <script type="text/javascript" src="js/controls/splitter.js"></script>
     <script type="text/javascript">
         var initialViewID = <%= initialViewID %>;
         var initialViewUrl = "<%= initialViewUrl %>";
---
+++ ScadaWeb/ScadaWebShell5Beta/js/api/viewhub.js
@@ -0,0 +1,74 @@
+﻿/*
+ * View hub provides data exchange between a view, data windows and the shell
+ * Author   : Mikhail Shiryaev
+ * Created  : 2016
+ * Modified : 2016
+ */
+
+/*
+ * Requires:
+ * - jquery
+ */
+
+// Rapid SCADA namespace
+var scada = scada || {};
+
+// View hub type
+scada.ViewHub = function () {
+    // Current view ID
+    this.currentViewID = 0;
+
+    // View window JavaScript object
+    this.viewWindow = null;
+
+    // Data window JavaScript object
+    this.dataWindow = null;
+};
+
+// Navigate to the specified view
+scada.ViewHub.prototype.navigate = function (viewID, viewUrl) {
+
+};
+
+// Get data window URL considering the current view
+scada.ViewHub.prototype.getDataWindowUrl = function (initialUrl) {
+
+};
+
+// Show chart web page
+scada.ViewHub.prototype.showChart = function (viewID, cnlNums) {
+
+};
+
+// Show command dialog
+scada.ViewHub.prototype.showCmdDialog = function (viewID, ctrlCnlNum) {
+
+};
+
+// Show event acknowledgement dialog
+scada.ViewHub.prototype.showEventAckDialog = function (viewID, year, month, day, evNum) {
+
+}
+
+// Add the specified view to the hub
+scada.ViewHub.prototype.addView = function (wnd) {
+
+};
+
+// Add the specified data window to the hub
+scada.ViewHub.prototype.addDataWindow = function (wnd) {
+
+};
+
+// Send notification to a view or data window
+scada.ViewHub.prototype.notify = function (senderWnd, eventType, opt_extraParams) {
+
+};
+
+// View hub locator object
+scada.viewHubLocator = {
+    // Find and return an existing view hub object
+    getViewHub: function () {
+
+    }
+};
