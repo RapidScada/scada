af519b1a321b2ed9028e513094ef01c65bc66f2e
2mik <m_shir@mail.ru>
2016-09-21 17:03:33 +0300

    PlgTable: start report

---
+++ ScadaWeb/OpenPlugins/PlgTable/plugins/Table/EventsRepOut.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scada.Web.Plugins.Table
+{
+    public partial class WFrmEventsRepOut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
