285f1415b79430eb8328b1a858460612466be673
2mik <m_shir@mail.ru>
2016-10-25 12:10:47 +0300

    Base: update to 4.2

--- Base/sql/convert_3.0_to_4.0.sql
+++ Base/sql/convert_3.0_to_4.0.sql
@@ -1,4 +1,4 @@
--- Преобразовать базу конфигурации из версии 3.0 в версию 4.0
+-- Convert the configuration database from v3.0 to v4.0
 CREATE TABLE Formula
 ( 
 	Name                 nvarchar(50)  NOT NULL ,
---
+++ Base/sql/convert_4.1_to_4.2.sql
@@ -0,0 +1,3 @@
+-- Script Date: 25.10.2016 12:05  - ErikEJ.SqlCeScripting version 3.5.2.58
+ALTER TABLE [KP] ALTER COLUMN [CallNum] nvarchar(50) NULL ;
+GO
