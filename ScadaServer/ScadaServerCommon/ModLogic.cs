18b72a6374ab9bb31ec31af4858e1ce71db8861d
2mik <m_shir@mail.ru>
2017-01-10 17:22:00 +0300

    ScadaServer: add module version prop

--- ScadaServer/ScadaServerCommon/ModLogic.cs
+++ ScadaServer/ScadaServerCommon/ModLogic.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2015 Mikhail Shiryaev
+ * Copyright 2017 Mikhail Shiryaev
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2013
- * Modified : 2015
+ * Modified : 2017
  */
 
 using Scada.Data.Models;
@@ -39,7 +39,7 @@ namespace Scada.Server.Modules
         /// <summary>
         /// Время ожидания остановки работы модуля, мс
         /// </summary>
-        protected const int WaitForStop = 7000;
+        public const int WaitForStop = 7000;
 
         private AppDirs appDirs; // директории приложения
 
