11eff311229c104732466f0cb21b92e2bc8a907b
2mik <m_shir@mail.ru>
2017-01-24 15:47:29 +0300

    ScadaDoc: modbus en

--- ScadaDoc/ScadaDoc/js/contents-en.js
+++ ScadaDoc/ScadaDoc/js/contents-en.js
@@ -31,9 +31,9 @@ function addContents(context) {
     addArticle(context, "", "Modules");
     addArticle(context, "modules/mod-db-export.html", "Export to Database Module", 1);
 
-    /*addArticle(context, "", "Use Cases");
+    addArticle(context, "", "Use Cases");
     addArticle(context, "use-cases/modbus-protocol.html", "Connecting Devices Using Modbus Protocol", 1);
-    addArticle(context, "use-cases/opc-standard.html", "Connecting Devices Using OPC Standard", 1);*/
+    /*addArticle(context, "use-cases/opc-standard.html", "Connecting Devices Using OPC Standard", 1);*/
 
     /*addArticle(context, "", "Version History");
     addArticle(context, "version-history/scada-history.html", "Rapid SCADA", 1);
