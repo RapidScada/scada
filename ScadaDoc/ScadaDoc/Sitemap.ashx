7fac6595b125c9520aefb51099d63ef8efd91862
2mik <m_shir@mail.ru>
2017-03-24 10:54:37 +0300

    ScadaDoc: dev sitemap

---
+++ ScadaDoc/ScadaDoc/Sitemap.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Sitemap.ashx.cs" Class="ScadaDoc.Sitemap" %>
---
+++ ScadaDoc/ScadaDoc/Sitemap.ashx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Web;
+using System.Xml;
+
+namespace ScadaDoc
+{
+    /// <summary>
+    /// Генерирует карту сайта для поисковых систем
+    /// </summary>
+    public class Sitemap : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            try
+            {
+                context.Response.ContentType = "text/xml";
+                string siteRoot = context.Request.Url.GetLeftPart(UriPartial.Authority);
+
+                using (XmlTextWriter writer = new XmlTextWriter(context.Response.OutputStream, Encoding.UTF8))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("urlset");
+                    writer.WriteAttributeString("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
+
+                    writer.WriteStartElement("url");
+                    writer.WriteElementString("loc", siteRoot);
+                    writer.WriteEndElement();
+
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.Write(ex.Message);
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
