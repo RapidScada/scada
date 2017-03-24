5311ff83959ee793c607576f55f7240f3aace38c
2mik <m_shir@mail.ru>
2017-03-24 16:56:18 +0300

    ScadaDoc: sitemap done

--- ScadaDoc/ScadaDoc/Sitemap.ashx.cs
+++ ScadaDoc/ScadaDoc/Sitemap.ashx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Web;
 using System.Xml;
@@ -15,7 +16,8 @@ namespace ScadaDoc
             try
             {
                 context.Response.ContentType = "text/xml";
-                string siteRoot = context.Request.Url.GetLeftPart(UriPartial.Authority);
+                string siteRoot = VirtualPathUtility.AppendTrailingSlash(
+                    context.Request.Url.GetLeftPart(UriPartial.Authority) + context.Request.ApplicationPath);
 
                 using (XmlTextWriter writer = new XmlTextWriter(context.Response.OutputStream, Encoding.UTF8))
                 {
@@ -23,9 +25,26 @@ namespace ScadaDoc
                     writer.WriteStartElement("urlset");
                     writer.WriteAttributeString("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
 
+                    // добавление корня сайта
                     writer.WriteStartElement("url");
                     writer.WriteElementString("loc", siteRoot);
                     writer.WriteEndElement();
+
+                    // добавление всех html-файлов из директории контента
+                    string contentDir = context.Server.MapPath("~/content/");
+                    DirectoryInfo dirInfo = new DirectoryInfo(contentDir);
+                    FileInfo[] fileInfoArr = dirInfo.GetFiles("*.html", SearchOption.AllDirectories);
+
+                    string rootDir = context.Server.MapPath("~/");
+                    int rootDirLen = rootDir.Length;
+
+                    foreach (FileInfo fileInfo in fileInfoArr)
+                    {
+                        string loc = siteRoot + fileInfo.FullName.Substring(rootDirLen).Replace('\\', '/');
+                        writer.WriteStartElement("url");
+                        writer.WriteElementString("loc", loc);
+                        writer.WriteEndElement();
+                    }
 
                     writer.WriteEndElement();
                     writer.WriteEndDocument();
