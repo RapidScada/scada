949267fe88fd11440184d92a113cd88245453a4c
2mik <m_shir@mail.ru>
2017-03-30 15:41:18 +0300

    ScadaWeb: update assembly info

--- ScadaWeb/ScadaWebShell5/Properties/AssemblyInfo.cs
+++ ScadaWeb/ScadaWebShell5/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScadaWebShell5Beta")]
+[assembly: AssemblyTitle("ScadaWebShell5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
