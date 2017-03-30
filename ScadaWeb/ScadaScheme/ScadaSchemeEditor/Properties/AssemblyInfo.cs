14e3c75cf6f31086374c0006db416633f35b8e25
2mik <m_shir@mail.ru>
2017-03-30 17:52:28 +0300

    ScadaScheme: dev 2

--- ScadaWeb/ScadaScheme/ScadaSchemeEditor/Properties/AssemblyInfo.cs
+++ ScadaWeb/ScadaScheme/ScadaSchemeEditor/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Scada.Scheme;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -9,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScadaSchemeEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Rapid SCADA")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(SchemeUtils.SchemeVersion)]
+[assembly: AssemblyFileVersion(SchemeUtils.SchemeVersion)]
