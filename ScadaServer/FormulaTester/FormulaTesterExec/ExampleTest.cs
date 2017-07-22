2d47cf09872525a658ae7f97ff58dca537e037e1
2mik <m_shir@mail.ru>
2017-07-22 15:49:06 +0300

    Initial commit of FormulaTester project

---
+++ ScadaServer/FormulaTester/FormulaTesterExec/ExampleTest.cs
@@ -0,0 +1,25 @@
+﻿using FormulaTester;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FormulaTesterExec
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod]
+        public void LinearTransformTest()
+        {
+            CustomFormulas customFormulas = new CustomFormulas();
+            double actualResult = customFormulas.LinearTransform(5, 10);
+            Assert.AreEqual(10, actualResult);
+        }
+
+        [TestMethod]
+        public void CnlValSumTest()
+        {
+            CustomFormulas customFormulas = new CustomFormulas();
+            double actualResult = customFormulas.CnlValSum(102, 102, 103, 104, 105);
+            Assert.AreEqual(15, actualResult);
+        }
+    }
+}
