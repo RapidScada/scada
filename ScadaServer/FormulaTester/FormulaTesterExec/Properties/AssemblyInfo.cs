8169cf60c206063967f55ee35fa2913d7206262c
2mik <m_shir@mail.ru>
2017-07-24 20:45:14 +0300

    FormulaTester: minor changes

--- ScadaServer/FormulaTester/FormulaTesterExec/Properties/AssemblyInfo.cs
+++ ScadaServer/FormulaTester/FormulaTesterExec/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FormulaTesterExec")]
+[assembly: AssemblyTitle("Formula Tester Executor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FormulaTesterExec")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Rapid SCADA")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
