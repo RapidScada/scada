918115c98d3517b26c1ef2be840d238031d50388
2mik <m_shir@mail.ru>
2017-08-30 11:32:56 +0300

    KpHttpNotif: share the project

---
+++ ScadaComm/OpenKPs/KpHttpNotif/HttpNotif/UI/FrmDevProps.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Scada.Comm.Devices.HttpNotif.UI
+{
+    partial class FrmDevProps
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblReqUrl = new System.Windows.Forms.Label();
+            this.txtReqUrl = new System.Windows.Forms.TextBox();
+            this.btnAddressBook = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblReqUrl
+            // 
+            this.lblReqUrl.AutoSize = true;
+            this.lblReqUrl.Location = new System.Drawing.Point(9, 9);
+            this.lblReqUrl.Name = "lblReqUrl";
+            this.lblReqUrl.Size = new System.Drawing.Size(72, 13);
+            this.lblReqUrl.TabIndex = 0;
+            this.lblReqUrl.Text = "Request URL";
+            // 
+            // txtReqUrl
+            // 
+            this.txtReqUrl.Location = new System.Drawing.Point(12, 25);
+            this.txtReqUrl.Multiline = true;
+            this.txtReqUrl.Name = "txtReqUrl";
+            this.txtReqUrl.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReqUrl.Size = new System.Drawing.Size(360, 100);
+            this.txtReqUrl.TabIndex = 1;
+            this.txtReqUrl.TextChanged += new System.EventHandler(this.txtReqUrl_TextChanged);
+            // 
+            // btnAddressBook
+            // 
+            this.btnAddressBook.Location = new System.Drawing.Point(12, 131);
+            this.btnAddressBook.Name = "btnAddressBook";
+            this.btnAddressBook.Size = new System.Drawing.Size(130, 23);
+            this.btnAddressBook.TabIndex = 2;
+            this.btnAddressBook.Text = "Address book";
+            this.btnAddressBook.UseVisualStyleBackColor = true;
+            this.btnAddressBook.Click += new System.EventHandler(this.btnAddressBook_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(216, 131);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(297, 131);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FrmDevProps
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(384, 166);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnAddressBook);
+            this.Controls.Add(this.txtReqUrl);
+            this.Controls.Add(this.lblReqUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmDevProps";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "HTTP Notifications - Device {0} Properties";
+            this.Load += new System.EventHandler(this.FrmDevProps_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblReqUrl;
+        private System.Windows.Forms.TextBox txtReqUrl;
+        private System.Windows.Forms.Button btnAddressBook;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ ScadaComm/OpenKPs/KpHttpNotif/KpHttpNotifLogic.cs
@@ -0,0 +1,411 @@
+﻿/*
+ * Copyright 2017 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : KpHttpNotif
+ * Summary  : Device communication logic
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2016
+ * Modified : 2017
+ */
+
+using Scada.Comm.Devices.AB;
+using Scada.Comm.Devices.HttpNotif;
+using Scada.Data.Configuration;
+using Scada.Data.Models;
+using Scada.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace Scada.Comm.Devices
+{
+    /// <summary>
+    /// Device communication logic
+    /// <para>Логика работы КП</para>
+    /// </summary>
+    public class KpHttpNotifLogic : KPLogic
+    {
+        /// <summary>
+        /// Состояния опроса КП
+        /// </summary>
+        private enum SessStates
+        {
+            /// <summary>
+            /// Фатальная ошибка
+            /// </summary>
+            FatalError,
+            /// <summary>
+            /// Ожидание команд
+            /// </summary>
+            Waiting
+        }
+
+        /// <summary>
+        /// Отправляемое уведомление
+        /// </summary>
+        private class Notification
+        {
+            /// <summary>
+            /// Конструктор
+            /// </summary>
+            public Notification()
+            {
+                PhoneNumbers = new List<string>();
+                Emails = new List<string>();
+                Text = "";
+            }
+
+            /// <summary>
+            /// Получить телефонные номера
+            /// </summary>
+            public List<string> PhoneNumbers { get; private set; }
+            /// <summary>
+            /// Получить адреса эл. почты
+            /// </summary>
+            public List<string> Emails { get; private set; }
+            /// <summary>
+            /// Получить или установить текст
+            /// </summary>
+            public string Text { get; set; }
+        }
+
+
+        // Имена переменных в командной строке для формирования запроса
+        private const string PhoneVarName = "phone";
+        private const string EmailVarName = "email";
+        private const string TextVarName = "text";
+        // Разделитель адресов в запросе
+        private const string ReqAddrSep = ";";
+        // Разделитель адресов в команде ТУ
+        private const string CmdAddrSep = ";";
+        // Длина буфера ответа на запрос
+        private const int RespBufLen = 100;
+        // Кодировка ответа на запрос
+        private static readonly Encoding RespEncoding = Encoding.UTF8;
+
+        private AB.AddressBook addressBook; // адресная книга, общая для линии связи
+        private SessStates sessState;       // состояние опроса КП
+        private bool writeSessState;        // вывести состояние опроса КП
+        private ParamString reqTemplate;    // шаблон запроса
+        private char[] respBuf;             // буфер ответа на запрос
+
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public KpHttpNotifLogic(int number)
+            : base(number)
+        {
+            CanSendCmd = true;
+            ConnRequired = false;
+
+            addressBook = null;
+            sessState = SessStates.Waiting;
+            writeSessState = true;
+            reqTemplate = null;
+            respBuf = new char[RespBufLen];
+
+            InitKPTags(new List<KPTag>()
+            {
+                new KPTag(1, Localization.UseRussian ? "Отправлено уведомлений" : "Sent notifications")
+            });
+        }
+
+
+        /// <summary>
+        /// Создать шаблон запроса
+        /// </summary>
+        private void CreateReqTemplate()
+        {
+            try
+            {
+                if (ReqParams.CmdLine == "")
+                {
+                    throw new ScadaException(Localization.UseRussian ?
+                        "Командная строка пуста." :
+                        "The command line is empty.");
+                }
+                else
+                {
+                    reqTemplate = new ParamString(ReqParams.CmdLine);
+                    ValidateReqTemplate();
+                    sessState = SessStates.Waiting;
+                }
+            }
+            catch (Exception ex)
+            {
+                sessState = SessStates.FatalError;
+                reqTemplate = null;
+                WriteToLog((Localization.UseRussian ?
+                    "Не удалось получить HTTP-запрос из командной строки КП: " :
+                    "Unable to get HTTP request from the device command line: ") + ex.Message);
+            }
+
+            writeSessState = true;
+        }
+
+        /// <summary>
+        /// Проверить шаблон запроса
+        /// </summary>
+        private void ValidateReqTemplate()
+        {
+            string reqUrl = reqTemplate.ToString();
+            WebRequest req = WebRequest.Create(reqUrl);
+
+            if (!(req is HttpWebRequest))
+            {
+                throw new ScadaException(Localization.UseRussian ?
+                    "Некорректный HTTP-запрос." :
+                    "Incorrect HTTP request.");
+            }
+        }
+
+        /// <summary>
+        /// Преобразовать состояние опроса КП в строку
+        /// </summary>
+        private string SessStateToStr()
+        {
+            switch (sessState)
+            {
+                case SessStates.FatalError:
+                    return Localization.UseRussian ?
+                        "Отправка уведомлений невозможна" :
+                        "Sending notifocations is impossible";
+                case SessStates.Waiting:
+                    return Localization.UseRussian ?
+                        "Ожидание команд..." :
+                        "Waiting for commands...";
+                default:
+                    return "";
+            }
+        }
+
+        /// <summary>
+        /// Попытаться получить уведомление из команды ТУ
+        /// </summary>
+        private bool TryGetNotif(Command cmd, out Notification notif)
+        {
+            string cmdDataStr = cmd.GetCmdDataStr();
+            int sepInd = cmdDataStr.IndexOf(CmdAddrSep);
+
+            if (sepInd >= 0)
+            {
+                string recipient = cmdDataStr.Substring(0, sepInd);
+                string text = cmdDataStr.Substring(sepInd + 1);
+
+                notif = new Notification() { Text = text };
+
+                if (addressBook == null)
+                {
+                    // добавление данных получателя, явно указанных в команде ТУ
+                    notif.PhoneNumbers.Add(recipient);
+                    notif.Emails.Add(recipient);
+                }
+                else
+                {
+                    // поиск адресов получателей в адресной книге
+                    AB.AddressBook.ContactGroup contactGroup = addressBook.FindContactGroup(recipient);
+                    if (contactGroup == null)
+                    {
+                        AB.AddressBook.Contact contact = addressBook.FindContact(recipient);
+                        if (contact == null)
+                        {
+                            // добавление данных получателя, явно указанных в команде ТУ
+                            notif.PhoneNumbers.Add(recipient);
+                            notif.Emails.Add(recipient);
+                        }
+                        else
+                        {
+                            // добавление данных получателя из контакта
+                            notif.PhoneNumbers.AddRange(contact.PhoneNumbers);
+                            notif.Emails.AddRange(contact.Emails);
+                        }
+                    }
+                    else
+                    {
+                        // добавление данных получателей из группы контактов
+                        foreach (AB.AddressBook.Contact contact in contactGroup.Contacts)
+                        {
+                            notif.PhoneNumbers.AddRange(contact.PhoneNumbers);
+                            notif.Emails.AddRange(contact.Emails);
+                        }
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                notif = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Отправить уведомление
+        /// </summary>
+        private bool SendNotif(Notification notif)
+        {
+            try
+            {
+                // получение адреса запроса
+                // использовать WebUtility.UrlEncode в .NET 4.5
+                reqTemplate.SetParam(PhoneVarName, Uri.EscapeDataString(string.Join(ReqAddrSep, notif.PhoneNumbers)));
+                reqTemplate.SetParam(EmailVarName, Uri.EscapeDataString(string.Join(ReqAddrSep, notif.Emails)));
+                reqTemplate.SetParam(TextVarName, Uri.EscapeDataString(notif.Text));
+                string reqUrl = reqTemplate.ToString();
+
+                // создание запроса
+                WebRequest req = WebRequest.Create(reqUrl);
+                req.Timeout = ReqParams.Timeout;
+
+                // отправка запроса и приём ответа
+                WriteToLog(Localization.UseRussian ?
+                    "Отправка уведомления. Запрос: " :
+                    "Send notification. Request: ");
+                WriteToLog(reqUrl);
+
+                using (WebResponse resp = req.GetResponse())
+                {
+                    using (Stream respStream = resp.GetResponseStream())
+                    {
+                        // чтение данных из ответа
+                        using (StreamReader reader = new StreamReader(respStream, RespEncoding))
+                        {
+                            int readCnt = reader.Read(respBuf, 0, RespBufLen);
+
+                            if (readCnt > 0)
+                            {
+                                string respString = new string(respBuf, 0, readCnt);
+                                WriteToLog(Localization.UseRussian ?
+                                    "Полученный ответ:" :
+                                    "Received response:");
+                                WriteToLog(respString);
+                            }
+                            else
+                            {
+                                WriteToLog(Localization.UseRussian ?
+                                    "Ответ не получен" :
+                                    "No response");
+                            }
+                        }
+                    }
+                }
+
+                IncCurData(0, 1); // увеличение счётчика уведомлений
+                return true;
+            }
+            catch (Exception ex)
+            {
+                WriteToLog((Localization.UseRussian ?
+                    "Ошибка при отправке уведомления: " :
+                    "Error sending notification: ") + ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Преобразовать данные тега КП в строку
+        /// </summary>
+        protected override string ConvertTagDataToStr(int signal, SrezTableLight.CnlData tagData)
+        {
+            if (tagData.Stat > 0)
+            {
+                if (signal == 1)
+                    return tagData.Val.ToString("N0");
+            }
+
+            return base.ConvertTagDataToStr(signal, tagData);
+        }
+
+
+        /// <summary>
+        /// Выполнить сеанс опроса КП
+        /// </summary>
+        public override void Session()
+        {
+            if (writeSessState)
+            {
+                writeSessState = false;
+                WriteToLog("");
+                WriteToLog(SessStateToStr());
+            }
+
+            Thread.Sleep(ScadaUtils.ThreadDelay);
+        }
+
+        /// <summary>
+        /// Отправить команду ТУ
+        /// </summary>
+        public override void SendCmd(Command cmd)
+        {
+            base.SendCmd(cmd);
+            lastCommSucc = false;
+
+            if (sessState == SessStates.FatalError)
+            {
+                WriteToLog(SessStateToStr());
+            }
+            else
+            {
+                if (cmd.CmdNum == 1 && cmd.CmdTypeID == BaseValues.CmdTypes.Binary)
+                {
+                    Notification notif;
+                    if (TryGetNotif(cmd, out notif))
+                    {
+                        if (SendNotif(notif))
+                            lastCommSucc = true;
+
+                        // задержка позволяет ограничить скорость отправки уведомлений
+                        Thread.Sleep(ReqParams.Delay);
+                    }
+                    else
+                    {
+                        WriteToLog(CommPhrases.IncorrectCmdData);
+                    }
+                }
+                else
+                {
+                    WriteToLog(CommPhrases.IllegalCommand);
+                }
+
+                writeSessState = true;
+            }
+
+            CalcCmdStats();
+        }
+
+        /// <summary>
+        /// Выполнить действия при запуске линии связи
+        /// </summary>
+        public override void OnCommLineStart()
+        {
+            // получение адресной книги
+            addressBook = AbUtils.GetAddressBook(AppDirs.ConfigDir, CommonProps, WriteToLog);
+            // создание шаблона запроса
+            CreateReqTemplate();
+            // сброс счётчика уведомлений
+            SetCurData(0, 0, 1);
+            // установка состояния работы КП
+            WorkState = sessState == SessStates.FatalError ? WorkStates.Error : WorkStates.Normal;
+        }
+    }
+}
