513a880cfeb980c36b606a90bbc62dab6287285b
2mik <m_shir@mail.ru>
2017-11-29 17:51:51 +0300

    ScadaScheme: component works!

--- ScadaWeb/ScadaScheme/PlgSchBasicComp/plugins/SchBasicComp/js/basiccomprender.js
+++ ScadaWeb/ScadaScheme/PlgSchBasicComp/plugins/SchBasicComp/js/basiccomprender.js
@@ -1,1 +1,8 @@
-﻿
+﻿scada.scheme.LedRenderer = function () {
+    scada.scheme.StaticTextRenderer.call(this);
+};
+
+scada.scheme.LedRenderer.prototype = Object.create(scada.scheme.StaticTextRenderer.prototype);
+scada.scheme.LedRenderer.constructor = scada.scheme.LedRenderer;
+
+scada.scheme.rendererMap.set("Scada.Web.Plugins.SchBasicComp.Led", new scada.scheme.LedRenderer());
