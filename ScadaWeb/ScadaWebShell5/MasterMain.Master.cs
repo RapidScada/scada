6df2d99f5ca670daaaa6b315933e2d5151d78084
2mik <m_shir@mail.ru>
2018-03-13 15:14:31 +0300

    ScadaWeb: choose http or https for stats

--- ScadaWeb/ScadaWebShell5/MasterMain.Master.cs
+++ ScadaWeb/ScadaWebShell5/MasterMain.Master.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2016 Mikhail Shiryaev
+ * Copyright 2018 Mikhail Shiryaev
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2016
- * Modified : 2016
+ * Modified : 2018
  */
 
 using Scada.UI;
@@ -86,7 +86,7 @@ namespace Scada.Web
         /// </summary>
         protected string GenStatsHtml()
         {
-            return appData.Stats.GenerateHtml(userData.WebSettings.ShareStats);
+            return appData.Stats.GenerateHtml(userData.WebSettings.ShareStats, Request.IsSecureConnection);
         }
 
 
