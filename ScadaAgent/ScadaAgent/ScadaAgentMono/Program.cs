94d9f3f15b3cddc3e0b1a628e47243be28dc8768
2mik <m_shir@mail.ru>
2018-03-19 16:27:33 +0300

    Initial commit of ScadaAgent

---
+++ ScadaAgent/ScadaAgent/ScadaAgentMono/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScadaAgentMono
+{
+    class Program
+    {
+        //private ServiceHost schemeEditorSvcHost; // хост WCF-службы для взаимодействия с веб-интерфейсом
+
+        /*private bool StartWcf()
+        {
+            try
+            {
+                ScadaSchemeSvc schemeSvc = new ScadaSchemeSvc();
+                schemeSvcHost = new ServiceHost(schemeSvc);
+                ServiceBehaviorAttribute behavior =
+                    schemeSvcHost.Description.Behaviors.Find<ServiceBehaviorAttribute>();
+                behavior.InstanceContextMode = InstanceContextMode.Single;
+                behavior.UseSynchronizationContext = false;
+                schemeSvcHost.Open();
+
+                domainSvcHost = new ServiceHost(typeof(CrossDomainSvc));
+                domainSvcHost.Open();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                log.WriteAction("Ошибка при запуске WCF-служб: " + ex.Message, Log.ActTypes.Exception);
+                ScadaUtils.ShowError(
+                    "Ошибка при запуске служб обмена данными.\nНормальная работа программы невозможна.");
+                return false;
+            }
+        }
+
+        private void StopWcf()
+        {
+            // остановка WCF-служб
+            if (schemeSvcHost != null)
+            {
+                try { schemeSvcHost.Close(); }
+                catch { schemeSvcHost.Abort(); }
+            }
+
+            if (domainSvcHost != null)
+            {
+                try { domainSvcHost.Close(); }
+                catch { domainSvcHost.Abort(); }
+            }
+        }*/
+
+        /*
+        /// <summary>
+        /// Запустить WCF-службу для взаимодействия с веб-интерфейсом
+        /// </summary>
+        private bool StartWcfService(out string serviceUrl)
+        {
+            try
+            {
+                schemeEditorSvcHost = new ServiceHost(typeof(SchemeEditorSvc));
+                ServiceBehaviorAttribute behavior =
+                    schemeEditorSvcHost.Description.Behaviors.Find<ServiceBehaviorAttribute>();
+                behavior.InstanceContextMode = InstanceContextMode.Single;
+                behavior.UseSynchronizationContext = false;
+                schemeEditorSvcHost.Open();
+                serviceUrl = schemeEditorSvcHost.BaseAddresses.Count > 0 ?
+                    schemeEditorSvcHost.BaseAddresses[0].AbsoluteUri : "";
+
+                Log.WriteAction(Localization.UseRussian ?
+                    "WCF-служба запущена" :
+                    "WCF service is started");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.WriteException(ex, Localization.UseRussian ?
+                    "Ошибка при запуске WCF-службы" :
+                    "Error starting WCF service");
+                serviceUrl = "";
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Остановить WCF-службу, взаимодействующую с веб-интерфейсом
+        /// </summary>
+        private void StopWcfService()
+        {
+            if (schemeEditorSvcHost != null)
+            {
+                try
+                {
+                    schemeEditorSvcHost.Close();
+                    Log.WriteAction(Localization.UseRussian ?
+                        "WCF-служба остановлена" :
+                        "WCF service is stopped");
+                }
+                catch
+                {
+                    schemeEditorSvcHost.Abort();
+                    Log.WriteAction(Localization.UseRussian ?
+                        "WCF-служба прервана" :
+                        "WCF service is aborted");
+                }
+
+                schemeEditorSvcHost = null;
+            }
+        }*/
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ScadaAgent/ScadaAgent/ScadaAgentWcf/AgentSvc.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace ScadaAgentWcf
+{
+    [ServiceContract]
+    public class AgentSvc
+    {
+        [OperationContract]
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
