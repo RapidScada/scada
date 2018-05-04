317dffb696b9106b1f905fa179860ce1e428fd39
2mik <m_shir@mail.ru>
2018-05-04 18:25:11 +0300

    ScadaAdmin: remote management

---
+++ ScadaAdmin/ScadaAdmin/Remote/FrmDownloadConfig.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ScadaAdmin.Remote
+{
+    partial class FrmDownloadConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDownloadConfig));
+            this.gbConnection = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBaseSDFFile = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.gbOptions = new System.Windows.Forms.GroupBox();
+            this.gbConnection.SuspendLayout();
+            this.gbOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbConnection
+            // 
+            this.gbConnection.Controls.Add(this.button3);
+            this.gbConnection.Controls.Add(this.button2);
+            this.gbConnection.Controls.Add(this.button1);
+            this.gbConnection.Controls.Add(this.comboBox1);
+            this.gbConnection.Location = new System.Drawing.Point(12, 12);
+            this.gbConnection.Name = "gbConnection";
+            this.gbConnection.Padding = new System.Windows.Forms.Padding(10, 3, 10, 10);
+            this.gbConnection.Size = new System.Drawing.Size(469, 55);
+            this.gbConnection.TabIndex = 0;
+            this.gbConnection.TabStop = false;
+            this.gbConnection.Text = "Подключение к серверу";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Настроить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(381, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(154, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Сохранить в директорию:";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(417, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnBaseSDFFile
+            // 
+            this.btnBaseSDFFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBaseSDFFile.Image = ((System.Drawing.Image)(resources.GetObject("btnBaseSDFFile.Image")));
+            this.btnBaseSDFFile.Location = new System.Drawing.Point(436, 43);
+            this.btnBaseSDFFile.Name = "btnBaseSDFFile";
+            this.btnBaseSDFFile.Size = new System.Drawing.Size(20, 20);
+            this.btnBaseSDFFile.TabIndex = 2;
+            this.btnBaseSDFFile.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 69);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(122, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Сохранить в архив:";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(436, 92);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(20, 20);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(417, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(325, 204);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Скачать";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(406, 204);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // gbOptions
+            // 
+            this.gbOptions.Controls.Add(this.radioButton1);
+            this.gbOptions.Controls.Add(this.textBox1);
+            this.gbOptions.Controls.Add(this.btnBaseSDFFile);
+            this.gbOptions.Controls.Add(this.button4);
+            this.gbOptions.Controls.Add(this.radioButton2);
+            this.gbOptions.Controls.Add(this.textBox2);
+            this.gbOptions.Location = new System.Drawing.Point(12, 73);
+            this.gbOptions.Name = "gbOptions";
+            this.gbOptions.Padding = new System.Windows.Forms.Padding(10, 3, 10, 10);
+            this.gbOptions.Size = new System.Drawing.Size(469, 125);
+            this.gbOptions.TabIndex = 1;
+            this.gbOptions.TabStop = false;
+            this.gbOptions.Text = "Параметры скачивания";
+            // 
+            // FrmDownloadConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(493, 239);
+            this.Controls.Add(this.gbOptions);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.gbConnection);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmDownloadConfig";
+            this.ShowInTaskbar = false;
+            this.Text = "Скачивание конфигурации";
+            this.gbConnection.ResumeLayout(false);
+            this.gbOptions.ResumeLayout(false);
+            this.gbOptions.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbConnection;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBaseSDFFile;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.GroupBox gbOptions;
+    }
+}
