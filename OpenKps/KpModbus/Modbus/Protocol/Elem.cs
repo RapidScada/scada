4dedc7fa8ca7d46b14e259071467bfb5306ffc68
Fatih Mehmet ARSLAN <iletisim@fmarslan.com>
2018-07-26 11:16:06 +0300

    Get bit in Holding Registers
    I developed Modbus.dll to get bits from byte of holding registers
    
    how to use :
    
    Step 1 : ElementGroup type selected Holding Registers
    Step 2 : ElementType selected bool
    Step 3 : We will write bitoffset value to Bit Offset Input Text

---
+++ OpenKps/KpModbus/Modbus/Protocol/Elem.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Copyright 2017 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : KpModbus
+ * Summary  : Modbus element (register)
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2012
+ * Modified : 2017
+ */
+
+using System;
+
+namespace Scada.Comm.Devices.Modbus.Protocol
+{
+    /// <summary>
+    /// Modbus element (register)
+    /// <para>Элемент (регистр) Modbus</para>
+    /// </summary>
+    public class Elem
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public Elem()
+        {
+            Name = "";
+            ElemType = ElemTypes.Bool;
+            ByteOrder = null;
+            ByteOrderStr = "";
+        }
+
+
+        /// <summary>
+        /// Получить или установить наименование
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Получить или установить тип
+        /// </summary>
+        public ElemTypes ElemType { get; set; }
+
+        /// <summary>
+        /// Получить длину элемента (количество адресов)
+        /// </summary>
+        public int Length
+        {
+            get
+            {
+                if (ElemType == ElemTypes.Bool)
+                {
+                    int offset = 0;
+                    if (Int32.TryParse(ByteOrderStr, out offset))
+                    {
+                        if (offset >= 32)
+                            return 4;
+                        if (offset >= 16)
+                            return 2;
+                        else
+                            return 1;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                else
+                {
+                    return ModbusUtils.GetElemCount(ElemType);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Получить или установить массив, определяющий порядок байт
+        /// </summary>
+        public int[] ByteOrder { get; set; }
+
+        /// <summary>
+        /// Получить или установить строковую запись порядка байт
+        /// </summary>
+        public string ByteOrderStr { get; set; }
+    }
+}
