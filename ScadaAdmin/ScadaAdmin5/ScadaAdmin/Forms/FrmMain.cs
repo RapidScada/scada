037c0f2bb347de8969f907cbf6d59fb6c284c48a
2mik <m_shir@mail.ru>
2018-08-01 11:56:54 +0300

    Initial commit of the new Administrator

---
+++ ScadaAdmin/ScadaAdmin5/ScadaAdmin/Forms/FrmMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scada.Admin.App
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ScadaAdmin/ScadaAdmin5/ScadaAdminCommon/AdminUtils.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright 2018 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : ScadaAdminCommon
+ * Summary  : The class contains utility methods for Administrator and its libraries
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2018
+ * Modified : 2018
+ */
+
+namespace Scada.Admin
+{
+    /// <summary>
+    /// The class contains utility methods for Administrator and its libraries
+    /// <para>Класс, содержащий вспомогательные методы для Администратора и его библиотек</para>
+    /// </summary>
+    public static class AdminUtils
+    {
+        /// <summary>
+        /// Version of Administrator
+        /// </summary>
+        public const string AppVersion = "5.1.0.4";
+    }
+}
