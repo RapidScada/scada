bc9406c337cae2cce7a4623563e236aada02c08e
2mik <m_shir@mail.ru>
2018-08-09 21:22:15 +0300

    ScadaData: generic DeepClone

--- ScadaData/ScadaData/ScadaUtils.cs
+++ ScadaData/ScadaData/ScadaUtils.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2017 Mikhail Shiryaev
+ * Copyright 2018 Mikhail Shiryaev
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2007
- * Modified : 2017
+ * Modified : 2018
  */
 
 using System;
@@ -335,6 +335,14 @@ namespace Scada
                 ms.Position = 0;
                 return bf.Deserialize(ms);
             }
+        }
+        
+        /// <summary>
+        /// Глубокое (полное) клонирование объекта
+        /// </summary>
+        public static T DeepClone<T>(T obj, SerializationBinder binder = null)
+        {
+            return (T)DeepClone(obj, binder);
         }
 
         /// <summary>
