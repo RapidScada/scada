85160861b0e8f3fa9f06cdad61e6fb5f234377fd
2mik <m_shir@mail.ru>
2018-08-21 12:17:31 +0300

    ScadaServer: move to another folder

--- ScadaData/ScadaData/ScadaUtils.cs
+++ ScadaData/ScadaData/ScadaUtils.cs
@@ -40,7 +40,7 @@ namespace Scada
         /// <summary>
         /// Версия данной библиотеки
         /// </summary>
-        internal const string LibVersion = "5.1.1.0";
+        internal const string LibVersion = "5.1.2.0";
         /// <summary>
         /// Формат вещественных чисел с разделителем точкой
         /// </summary>
