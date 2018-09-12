1ee88bcabb20bf4ffd550de0c44b0d23439904b5
2mik <m_shir@mail.ru>
2018-09-12 14:21:02 +0300

    ScadaAgent: change solution structure

---
+++ ScadaAgent/ScadaAgentConnector/AgentWcfClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scada.Agent.Connector
+{
+    public class AgentWcfClient
+    {
+    }
+}
