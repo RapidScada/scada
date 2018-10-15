56d666417406af4e9792756f68fbf6e84997130c
2mik <m_shir@mail.ru>
2018-10-15 12:37:21 +0300

    ScadaAgent: fix namespace

--- ScadaAgent/ScadaAgentCommon.UI/RemoteLogBox.cs
+++ ScadaAgent/ScadaAgentCommon.UI/RemoteLogBox.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ScadaAgentCommon.UI
+namespace Scada.Agent.UI
 {
     public class RemoteLogBox : LogBox
     {
