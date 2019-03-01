87217a1e1bcb7ec0277cfc224b94eaa5361f84fe
2mik <m_shir@mail.ru>
2019-03-01 16:04:12 +0300

    Add N(n) funtion

--- ScadaServer/ScadaServer/ScadaServerCommon/ServerUtils.cs
+++ ScadaServer/ScadaServer/ScadaServerCommon/ServerUtils.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2018 Mikhail Shiryaev
+ * Copyright 2019 Mikhail Shiryaev
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2015
- * Modified : 2018
+ * Modified : 2019
  */
 
 using Scada.Data.Tables;
@@ -39,7 +39,7 @@ namespace Scada.Server
         /// <summary>
         /// Версия Сервера
         /// </summary>
-        public const string AppVersion = "5.1.1.1";
+        public const string AppVersion = "5.1.2.0";
         /// <summary>
         /// Старший байт номера версии Сервера
         /// </summary>
