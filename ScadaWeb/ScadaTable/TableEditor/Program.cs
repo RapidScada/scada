a0c274cbe33543feb93d68a3a68d5f1b23af245f
2mik <m_shir@mail.ru>
2019-04-11 14:28:01 +0300

    Initial commit of the new Table Editor

---
+++ ScadaWeb/ScadaTable/TableEditor/Program.cs
@@ -0,0 +1,20 @@
+﻿using Scada.Table.Editor.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace Scada.Table.Editor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+    }
+}
