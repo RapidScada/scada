41ad2aa9fe4f4ece0f952b21a18785f805206dd6
2mik <m_shir@mail.ru>
2020-01-27 12:15:57 +0300

    Remove outdated configurations

--- Config/DefaultMono/SCADA/scadastart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Starting Rapid SCADA..."
-sudo service scadaserver start
-sudo service scadacomm start
--- Config/DefaultMono/SCADA/svc_uninstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo update-rc.d -f scadaserver remove
-sudo update-rc.d -f scadacomm remove
