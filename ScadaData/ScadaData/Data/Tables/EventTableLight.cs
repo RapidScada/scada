3db6d18bac35012336896f0ea81eef6ae3973efd
2mik <m_shir@mail.ru>
2020-02-11 17:44:18 +0300

    ScadaData: Serializable attr for Event

--- ScadaData/ScadaData/Data/Tables/EventTableLight.cs
+++ ScadaData/ScadaData/Data/Tables/EventTableLight.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2018 Mikhail Shiryaev
+ * Copyright 2020 Mikhail Shiryaev
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2007
- * Modified : 2018
+ * Modified : 2020
  */
 
 using System;
@@ -37,6 +37,7 @@ namespace Scada.Data.Tables
         /// <summary>
         /// Данные события
         /// </summary>
+        [Serializable]
         public class Event
         {
             /// <summary>
