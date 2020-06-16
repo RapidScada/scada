23fe98922c9b8d456fcec75c5fd4f34ae279b096
2mik <m_shir@mail.ru>
2020-06-16 17:37:41 +0300

    ScadaComm: fix reading lines on Linux

--- ScadaComm/ScadaComm/ScadaCommCommon/CommUtils.cs
+++ ScadaComm/ScadaComm/ScadaCommCommon/CommUtils.cs
@@ -67,7 +67,7 @@ namespace Scada.Comm
         /// <summary>
         /// Версия Коммуникатора
         /// </summary>
-        public const string AppVersion = "5.2.1.0";
+        public const string AppVersion = "5.2.1.1";
         /// <summary>
         /// Формат даты и времени для вывода в журнал линии связи
         /// </summary>
